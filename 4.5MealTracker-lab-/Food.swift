2fc1970d794c9f3af7e3d031f5e884444910df96
codegangsk <kjhj0604@gmail.com>
2020-09-10 16:05:44 +0900

    create Meal and Food struct

---
+++ 4.5MealTracker-lab-/Food.swift
@@ -0,0 +1,15 @@
+//
+//  Food.swift
+//  4.5MealTracker-lab-
+//
+//  Created by Sophie Kim on 2020/09/10.
+//  Copyright © 2020 Sophie Kim. All rights reserved.
+//
+
+import Foundation
+
+struct Food {
+    var name: String
+    var description: String
+}
+
---
+++ 4.5MealTracker-lab-/Meal.swift
@@ -0,0 +1,14 @@
+//
+//  Meal.swift
+//  4.5MealTracker-lab-
+//
+//  Created by Sophie Kim on 2020/09/10.
+//  Copyright © 2020 Sophie Kim. All rights reserved.
+//
+
+import Foundation
+
+struct Meal {
+    var name: String
+    var food: [Food]
+}
